9f8bb72f2dd8bf74d9277e92f36ea83701011fcb
zhang2172268 <zhang2172268@163.com>
2019-02-10 19:35:56 +0800

    字符流中第一个不重复的字符

---
+++ test_FirstAppearingOnce.cpp
@@ -0,0 +1,33 @@
+/* 字符流中第一个不重复的字符
+ 
+   题目描述：
+       请实现一个函数用来找出字符流中第一个只出现一次的字符。例如，当从字符流中只读出前两个字符"go"时，第一个只出现一次的字符是"g"。当从该字符流中读出前六个字符“google"时，第一个只出现一次的字符是"l"。
+
+   输出描述:
+       如果当前字符流没有存在出现一次的字符，返回#字符。
+ */
+
+class Solution
+{
+public:
+  //Insert one char from stringstream
+    void Insert(char ch)
+    {
+        str+=ch;
+        count[ch]++;
+    }
+  //return the first appearence once char in current stringstream
+    char FirstAppearingOnce()
+    {
+        int len=str.size();
+        for(int i=0;i<len;i++){
+            if(count[str[i]]==1)
+                return str[i];
+        }
+        return '#';
+    }
+
+private:
+    string str;
+    int count[256]={0};
+};
